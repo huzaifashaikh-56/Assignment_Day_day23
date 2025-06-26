0e84d0a38c781de8e051cd53e01ee60d973493d3
huzaifashaikh-56 <hs7030342@gmail.com>
2025-06-26 16:57:41 +0530

    Update connect.php

--- Day23_assignment_huzaifa/connect.php
+++ Day23_assignment_huzaifa/connect.php
@@ -1,1 +1,12 @@
+<?php
+// Step 2: Connect to MySQL database
+$conn = new mysqli("localhost", "root", "", "webteam_intern");
+
+// Check if connection is successful
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected successfully!";
+}
+?>
 
